93d62853735cc3008ba44df3e94c5edf715d5328
animmore <zaskokazhenya@gmail.com>
2019-11-08 16:59:05 +0200

    git init

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View>
+        <Text> Hello, Papery!</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
