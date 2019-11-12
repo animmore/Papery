3af86b8725c6f53748b342d6259202186fc55d9b
animmore <zaskokazhenya@gmail.com>
2019-11-12 13:25:49 +0200

    Fetch, search contacts

--- src/components/ContactsScreen/ContactsScreen.js
+++ src/components/ContactsScreen/ContactsScreen.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from 'react'
+import {View, Text, Image, TouchableOpacity, TextInput, StyleSheet, FlatList} from 'react-native'
+import {useContactsScreen} from './useContactsScreen'
 
-const ContactsScreen = () => {
+const ContactsScreen = (props) => {
+  const {filtratedContacts, searchContacts} = useContactsScreen()
+
+  renderItem = ({item, index}) => {
+    return (
+      <TouchableOpacity>
+        <View style={styles.contacts}>
+          <View>
+            <Image source={{uri: item.picture.large}} style={styles.contactImage} />
+          </View>
+          <View>
+            <Text style={styles.nameContact}>
+              {item.name.first} {item.name.last}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}> Contacts Screen </Text>
+      <TextInput
+        style={styles.search}
+        placeholder={'Search..'}
+        placeholderTextColor={'gray'}
+        onChangeText={searchContacts()}
+      />
+      <View style={styles.containerContacts}>
+        <FlatList
+          windowSize={10}
+          data={filtratedContacts}
+          renderItem={renderItem}
+          keyExtractor={(filtratedContacts, index) => `${index}`}
+        />
+      </View>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
     backgroundColor: '#FFF',
+  },
+  contacts: {
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    marginTop: 25,
+  },
+  nameContact: {
+    alignSelf: 'center',
+    height: 50,
+  },
+  search: {
+    flex: 0.5,
+    alignSelf: 'stretch',
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+  },
+  containerContacts: {
+    flex: 5,
   },
   textStyle: {
     fontSize: 24,
     color: 'gray',
     backgroundColor: '#FFF',
   },
-});
+})
 
-export default ContactsScreen;
+export default ContactsScreen
---
+++ src/components/ContactsScreen/useContactsScreen.js
@@ -0,0 +1,41 @@
+//@flow
+
+import {useState, useEffect} from 'react'
+
+export const useContactsScreen = () => {
+  const [contacts, setContacts] = useState([])
+  const [filtratedContacts, setFiltratedContacts] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+
+  fetchContacts = () => {
+    fetch('https://randomuser.me/api/?results=20&inc=name,picture,dob,email,phone')
+      .then((response) => response.json())
+      .then((response) => {
+        setContacts(response.results)
+        setFiltratedContacts(response.results)
+      })
+  }
+
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  newArrOfContacts = () => {
+    const newContacts: any = contacts.filter(
+      (val) =>
+        val.name.first.toLowerCase().includes(searchInput.toLowerCase()) ||
+        val.name.last.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    setFiltratedContacts(newContacts)
+  }
+
+  useEffect(() => {
+    newArrOfContacts()
+  }, [searchInput])
+
+  searchContacts = () => {
+    return (key: string) => setSearchInput(key)
+  }
+
+  return {filtratedContacts, searchContacts}
+}
